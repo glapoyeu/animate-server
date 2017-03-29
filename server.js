405212cd0fc32524e04b97cdbfec71f4c936ccc4
genarolapoyeu <glapoyeu@researchmobile.co>
2017-03-29 13:25:22 -0600

    add server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const http = require('http')
+const port = process.env.PORT || 8080
+
+//const server = http.createServer(onRequest);
+const server = http.createServer()
+
+server.on('request', onRequest)
+server.on('listening', onListening)
+
+//server.listen(port, onListening)
+server.listen(port)
+
+function onRequest(req, res){
+	res.end('Hello io.js')
+}
+
+function onListening(){
+	console.log('Server running in port ' + port)
+}
+
+
